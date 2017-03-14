81d4f10bd26f70e8136e382b453c673a387a3ab9
Daniel Bastos <you@example.com>
2017-03-14 20:56:25 -0300

    PR review. Add index.html

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def webprint():
-    return render_template('mac_addresses.html') 
+    return render_template('index.html') 
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 3000)
