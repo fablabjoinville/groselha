07582027513839eddf21a6a3615d509b9282f007
Dbastos1710 <daniel.bastos1710@gmail.com>
2017-03-20 14:50:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def webprint():
-    return render_template('index.html') 
+    return(render_template('index.html'))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
