43ab4fd2733715f486417f84a9e8e5db40d006d4
Dbastos1710 <daniel.bastos1710@gmail.com>
2017-03-20 14:49:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -6,5 +7,6 @@ app = Flask(__name__)
 def webprint():
     return render_template('index.html') 
 
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 3000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
