695d01ae6323dcaf7a218f4f7a83145e531ed74e
Daniel Bastos <you@example.com>
2017-03-21 19:15:55 -0300

    try try

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 import os
+import sys
+import logging
 
 app = Flask(__name__)
 
@@ -10,3 +12,7 @@ def webprint():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
