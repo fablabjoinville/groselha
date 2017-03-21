2b613b6829c71d34bab74f9571c1e27942074bb2
Daniel Bastos <you@example.com>
2017-03-21 20:16:19 -0300

    Change default port

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def webprint():
     return(render_template('index.html'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
