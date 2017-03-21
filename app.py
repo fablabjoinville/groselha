20b81fe33d0af201fd7210a123360f7342e045bd
Daniel Bastos <you@example.com>
2017-03-21 19:36:15 -0300

    second

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 import os
-import sys
-import logging
+
 
 app = Flask(__name__)
 
@@ -10,9 +9,5 @@ def webprint():
     return(render_template('index.html'))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
 
-
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
