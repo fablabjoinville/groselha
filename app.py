d81fe16eda36d3a5fa23d163de27bd46f84c4815
Daniel Bastos <you@example.com>
2017-03-21 20:20:03 -0300

    Return text message on /

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def webprint():
-    return(render_template('index.html'))
+    return 'Hello world!'
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
