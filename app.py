2ab445982a38956ccd87c7a2f59c335a9e17011f
Daniel Bastos <you@example.com>
2017-03-21 21:48:20 -0300

    Add mock endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 
@@ -7,7 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def webprint():
     return render_template('index.html')
-   # return 'Hello world!'
+
+@app.route('/macs', methods=['POST'])
+def save_macs():
+	r = request.get_json()
+	return str(r)
+
+@app.route('/who', methods=['GET'])
+def who_is_in_the_room():
+	return 'WHO'
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
