c91e258c525396f697860e6ccb0d88ef2b446899
Daniel Bastos <you@example.com>
2017-03-25 11:53:17 -0300

    fix port

--- app.py
+++ app.py
@@ -9,5 +9,6 @@ def index():
     return(render_template("index.html"))
 
 
-if __name__ == ('__main__'):
-    app.run(host = '0.0.0.0', port = 5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
