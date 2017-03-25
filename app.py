eb9c3093a811e9d40b84a0dbc0e64c5561b268fc
Daniel Bastos <you@example.com>
2017-03-25 14:19:16 -0300

    testing endpoints

--- app.py
+++ app.py
@@ -9,6 +9,16 @@ def index():
     return(render_template("index.html"))
 
 
+@app.route('/macs', methods=['POST'])
+def save_macs():
+	r = request.get_json()
+	return (str(r))
+
+@app.route('/who', methods=['GET'])
+def who_is_in_the_room():
+	return ('WHO')
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
